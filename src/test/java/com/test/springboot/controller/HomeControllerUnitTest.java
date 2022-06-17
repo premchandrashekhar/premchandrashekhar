88f8366d239f2116309a1202a59cf1b4dca2f3c5
Prem Chandra Shekhar <pcshekhar.sonu@gmail.com>
2022-06-17 23:38:04 +0530

    adding files

--- src/test/java/com/test/springboot/controller/HomeControllerUnitTest.java
+++ src/test/java/com/test/springboot/controller/HomeControllerUnitTest.java
@@ -1,11 +1,7 @@
 package com.test.springboot.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(HomeController.class)
 public class HomeControllerUnitTest {
 
 	public HomeControllerUnitTest() {
